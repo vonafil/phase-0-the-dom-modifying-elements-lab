b9770840e39343a70b857d6a02b7870c260c5b8d
Alexei Lifanov <alexeilifanov@alexeis-air.myfiosgateway.com>
2021-07-28 16:15:42 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = 'YOUR-NAME is the champion'
